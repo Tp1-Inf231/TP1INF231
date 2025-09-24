bb4fe8d8d24b16cc32e6dd9ba9c198632c913a98
raven6712 <jordanelle226@gmail.com>
2025-09-24 19:30:27 +0100

    Add files via upload
    Le code ci-après permet d'effectuer une recherche séquentielle dans un tableau

---
+++ seq.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, x, found = 0;
+    printf("Taille du tableau : ");
+    scanf("%d", &n);
+
+    int T[n];
+    printf("Entrez les elements :\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &T[i]);
+
+    printf("Element a rechercher : ");
+    scanf("%d", &x);
+
+    for (int i = 0; i < n; i++) {
+        if (T[i] == x) {
+            printf("Element %d trouve a la position %d\n", x, i);
+            found = 1;
+            break;
+        }
+    }
+    if (!found)
+        printf("Element non trouve.\n");
+
+    return 0;
+}
